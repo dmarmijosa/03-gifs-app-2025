90961f1022cbf70b67ccec6d49e49c848548016f
Danny Armijos <daniel2667@hotmail.es>
2025-03-26 22:35:59 +0100

    agregar soporte para entornos de desarrollo y personalización del menú lateral

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '@enviroments/environment';
+
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './gifs-side-menu-header.component.html',
 })
-export class GifsSideMenuHeaderComponent { }
+export class GifsSideMenuHeaderComponent {
+  env = environment;
+}
