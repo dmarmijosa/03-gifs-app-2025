029bb1114ae4e8596a6c5656cacbbd7e9d21a751
Danny Armijos <daniel2667@hotmail.es>
2025-03-26 22:20:04 +0100

    agregar opciones de menú dinámicas y soporte para Font Awesome en el menú lateral

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs populares',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      subLabel: 'Buscar gifs',
+    },
+  ];
+}
