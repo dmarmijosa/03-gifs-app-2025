a06944667ed35d36d26d33833e9fa8602bf3604b
Danny Armijos <daniel2667@hotmail.es>
2025-03-26 21:36:19 +0100

    configurar Tailwind CSS y agregar páginas de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page.component').then(
+        (c) => c.DashboardPageComponent
+      ),
+    children: [
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/search-page/search-page.component').then(
+            (c) => c.SearchPageComponent
+          ),
+      },
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page.component').then(
+            (c) => c.TrendingPageComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      }
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
