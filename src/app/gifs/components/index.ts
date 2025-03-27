e1be8db2a7f736e2620fb00fc02f9256424018de
Danny Armijos <daniel2667@hotmail.es>
2025-03-27 15:39:14 +0100

    agregar componente GifList y GifListItem, y actualizar las páginas de dashboard y trending

--- src/app/gifs/components/index.ts
+++ src/app/gifs/components/index.ts
@@ -1,1 +1,2 @@
+export { GifListComponent } from './gif-list/gif-list.component';
 export { GifsSideMenuComponent } from "./gifs-side-menu/gifs-side-menu.component";
--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GifListComponent } from '../../components';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './trending-page.component.html'
 })
 export class TrendingPageComponent { }
