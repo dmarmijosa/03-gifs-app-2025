f164f12cab9db23359fb68e0781f1e9ad4025c8d
Danny Armijos <daniel2667@hotmail.es>
2025-03-27 17:15:21 +0100

    eliminar archivos de configuración de entorno obsoletos

--- src/environments/environment_template.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  companyName: 'Gifs',
-  companyName2: 'App',
-  companySlogan: 'Maneja tus gifs',
-  giphyApikey: null,
-  publicUrl: 'https://api.giphy.com/v1'
-};
