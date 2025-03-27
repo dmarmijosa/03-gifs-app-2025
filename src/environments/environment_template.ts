1edda0fe045dd3222838a3cd389696ffc803c217
Danny Armijos <daniel2667@hotmail.es>
2025-03-27 17:17:26 +0100

    agregar plantilla de configuración de entorno y ajustar .gitignore para incluir archivos específicos

---
+++ src/environments/environment_template.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+  giphyApikey: null,
+  publicUrl: 'https://api.giphy.com/v1'
+};
