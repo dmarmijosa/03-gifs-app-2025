02a1e92629d67be00d914c82f611548967a9ff7f
Danny Armijos <daniel2667@hotmail.es>
2025-03-28 10:46:03 +0100

    agregar servicio para gestionar el estado de desplazamiento; implementar recuperación del estado en la página de tendencias

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,14 +1,26 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gif.service';
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
   //imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
-export class TrendingPageComponent {
-  scrollDivRef = viewChild<ElementRef>('groupDiv');
+export class TrendingPageComponent implements AfterViewInit{
+
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
+
+  scrollDivRef = viewChild<ElementRef>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv =  this.scrollDivRef()?.nativeElement;
+    if(!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
+
 
   onScroll(event: Event) {
     const scrollDiv =  this.scrollDivRef()?.nativeElement;
@@ -20,11 +32,14 @@ export class TrendingPageComponent {
     const scrollHeight = scrollDiv.scrollHeight;
 
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
-
+    this.scrollStateService.trendingScrollState.set(scrollTop);
     if(isAtBottom) {
       this.gifService.loadTrendingGifs();
     }
 
+
+
+    console.log('scrollTop', scrollTop, this.scrollStateService.trendingScrollState());
     // console.log(isAtBottom);
     // console.log({scrolltotal: scrollTop + clientHeight,scrollHeight});
   }
